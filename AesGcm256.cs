7fbf26496c57be239810d21013d030854f316221
Kashif Mushtaq <45662405+KashifMushtaq@users.noreply.github.com>
2019-01-13 22:12:42 -0500

    C# version of Cross Platform AES 256 GCM Encryption / Decryption
    Cross Platform AES 256 GCM Encryption / Decryption (C#) Class and test console application

---
+++ AesGcm256.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Text;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+
+namespace TestAES_GCM_256
+{
+
+    public class AesGcm256
+    {
+        private static readonly SecureRandom Random = new SecureRandom();
+
+        // Pre-configured Encryption Parameters
+        public static readonly int NonceBitSize = 128;
+        public static readonly int MacBitSize = 128;
+        public static readonly int KeyBitSize = 256;
+
+        private AesGcm256() { }
+
+        public static byte[] NewKey()
+        {
+            var key = new byte[KeyBitSize / 8];
+            Random.NextBytes(key);
+            return key;
+        }
+
+        public static byte[] NewIv()
+        {
+            var iv = new byte[NonceBitSize / 8];
+            Random.NextBytes(iv);
+            return iv;
+        }
+
+        public static Byte[] HexToByte(string hexStr)
+        {
+            byte[] bArray = new byte[hexStr.Length / 2];
+            for (int i = 0; i < (hexStr.Length / 2); i++)
+            {
+                byte firstNibble = Byte.Parse(hexStr.Substring((2 * i), 1), System.Globalization.NumberStyles.HexNumber); // [x,y)
+                byte secondNibble = Byte.Parse(hexStr.Substring((2 * i) + 1, 1), System.Globalization.NumberStyles.HexNumber);
+                int finalByte = (secondNibble) | (firstNibble << 4); // bit-operations only with numbers, not bytes.
+                bArray[i] = (byte)finalByte;
+            }
+            return bArray;
+        }
+
+
+        public static string toHex(byte[] data)
+        {
+            string hex = string.Empty;
+            foreach (byte c in data)
+            {
+                hex += c.ToString("X2");
+            }
+            return hex;
+        }
+
+
+        public static string toHex(string asciiString)
+        {
+            string hex = string.Empty;
+            foreach (char c in asciiString)
+            {
+                int tmp = c;
+                hex += string.Format("{0:x2}", System.Convert.ToUInt32(tmp.ToString()));
+            }
+            return hex;
+        }
+
+        public static string encrypt(string PlainText, byte[] key, byte[] iv)
+        {
+            string sR = string.Empty;
+            try
+            {
+                byte[] plainBytes = Encoding.UTF8.GetBytes(PlainText);
+
+                GcmBlockCipher cipher = new GcmBlockCipher(new AesFastEngine());
+                AeadParameters parameters = new AeadParameters(new KeyParameter(key), 128, iv, null);
+
+                cipher.Init(true, parameters);
+
+                byte[] encryptedBytes = new byte[cipher.GetOutputSize(plainBytes.Length)];
+                Int32 retLen = cipher.ProcessBytes(plainBytes, 0, plainBytes.Length, encryptedBytes, 0);
+                cipher.DoFinal(encryptedBytes, retLen);
+                sR = Convert.ToBase64String(encryptedBytes, Base64FormattingOptions.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return sR;
+        }
+
+
+        public static string decrypt(string EncryptedText, byte[] key, byte[] iv)
+        {
+            string sR = string.Empty;
+            try
+            {
+                byte[] encryptedBytes = Convert.FromBase64String(EncryptedText);
+
+                GcmBlockCipher cipher = new GcmBlockCipher(new AesFastEngine());
+                AeadParameters parameters = new AeadParameters(new KeyParameter(key), 128, iv, null);
+                //ParametersWithIV parameters = new ParametersWithIV(new KeyParameter(key), iv);
+
+                cipher.Init(false, parameters);
+                byte[] plainBytes = new byte[cipher.GetOutputSize(encryptedBytes.Length)];
+                Int32 retLen = cipher.ProcessBytes(encryptedBytes, 0, encryptedBytes.Length, plainBytes, 0);
+                cipher.DoFinal(plainBytes, retLen);
+
+                sR = Encoding.UTF8.GetString(plainBytes).TrimEnd("\r\n\0".ToCharArray());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return sR;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace TestAES_GCM_256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Generate and dump KEY so we could use again
+            //Console.WriteLine(AesGcm256.toHex(AesGcm256.NewKey()));
+
+            //Generate and dump IV so we could use again
+            //Console.WriteLine(AesGcm256.toHex(AesGcm256.NewIv()));
+
+            //Console.ReadKey();
+
+
+            //using above code these key and iv was generated
+            string hexKey = "2192B39425BBD08B6E8E61C5D1F1BC9F428FC569FBC6F78C0BC48FCCDB0F42AE";
+            string hexIV = "E1E592E87225847C11D948684F3B070D";
+
+            string plainText = "Test encryption and decryption";
+            Console.WriteLine("Plain Text: " + plainText);
+
+            //encrypt - result base64 encoded string
+            string encryptedText = AesGcm256.encrypt(plainText, AesGcm256.HexToByte(hexKey), AesGcm256.HexToByte(hexIV));
+            Console.WriteLine("Encrypted base64 encoded: " + encryptedText);
+
+            //decrypt - result plain string
+            string decryptedText = AesGcm256.decrypt(encryptedText, AesGcm256.HexToByte(hexKey), AesGcm256.HexToByte(hexIV));
+            Console.WriteLine("Decrypted Text: " + decryptedText);
+
+            if (plainText.Equals(decryptedText))
+            {
+                Console.WriteLine("Test Passed");
+            }
+            else
+            {
+                Console.WriteLine("Test Failed");
+            }
+
+            DeriveKey();
+
+            /* Console Output
+            Plain Text: Test encryption and decryption
+            Encrypted base64 encoded: A/boAixWJKflKviHp2cfDl6l/xn1qw2MsHcKFkrOfm2XOVmawIFct4fS1w7wKw==
+            Decrypted Text: Test encryption and decryption
+            Test Passed
+            Press any key to continue . . .
+            */
+        }
+        /// <summary>
+        /// Test to demonstrate that key and iv can be derived again using password if salt stays the same
+        /// </summary>
+        private static void DeriveKey()
+        {
+
+            String password = "Test@Password!";
+            byte[] saltBytes = new byte[8];
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                // Fill the array with a random value.
+                rngCsp.GetBytes(saltBytes);
+            }
+
+            Rfc2898DeriveBytes encryptionKey = new Rfc2898DeriveBytes(password, saltBytes, 10);
+            byte[] key1 = encryptionKey.GetBytes(32);
+            byte[] iv1 = encryptionKey.GetBytes(16);
+
+            encryptionKey = new Rfc2898DeriveBytes(password, saltBytes, 10);
+            byte[] key2 = encryptionKey.GetBytes(32);
+            byte[] iv2 = encryptionKey.GetBytes(16);
+
+
+            string plainText = "Test encryption and decryption";
+            Console.WriteLine("Plain Text: " + plainText);
+
+            //encrypt - result base64 encoded string
+            string encryptedText = AesGcm256.encrypt(plainText, key1, iv1);
+            Console.WriteLine("Encrypted base64 encoded: " + encryptedText);
+
+            //decrypt - result plain string
+            string decryptedText = AesGcm256.decrypt(encryptedText, key2, iv2);
+            Console.WriteLine("Decrypted Text: " + decryptedText);
+        }
+    }
+}
